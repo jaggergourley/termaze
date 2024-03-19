6cbfa9d1d79ce4be2e425996b7a8ad22504ffa64
Jagger Gourley <jaggergourley@gmail.com>
2024-03-18 21:28:59 -0500

    Printing initial structure to terminal

--- termaze.c
+++ termaze.c
@@ -1,6 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main() {
-    printf("Hello World!\n");
+#define ROWS 24
+#define COLS 80
+
+int main()
+{
+    // 80 x 24
+    char terminal[ROWS][COLS];
+
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+        {
+            terminal[i][j] = '*';
+        }
+    }
+
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+        {
+            printf("%c", terminal[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
